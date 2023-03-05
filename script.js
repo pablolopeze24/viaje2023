ec37b0683db742cb4dfd65472ae59fc23f1b3761
Pablo A. López <122939190+pablolopeze24@users.noreply.github.com>
2023-03-05 15:36:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -71,81 +71,81 @@ function updateTime() {
 
 setInterval(updateTime, 1000);
 
-// myElemento.classList.add('nombreclase');
+// // myElemento.classList.add('nombreclase');
 
 
-// Cuenta Regresiva
+// // Cuenta Regresiva
 
-// Set the date we're counting down to
-var countDownDate = new Date("March 27, 2023 6:00:00").getTime();
+// // Set the date we're counting down to
+// var countDownDate = new Date("March 27, 2023 6:00:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+// // Update the count down every 1 second
+// var x = setInterval(function() {
 
-  // Get the current time
-  var now = new Date().getTime();
+//   // Get the current time
+//   var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
 
-  // Calculate days, hours, minutes and seconds left
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  // var days = 9;
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//   // Calculate days, hours, minutes and seconds left
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   // var days = 9;
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Resultado para Argentina
-    let daysArg = days - 21;
-    if (daysArg > 0) {
-      document.getElementById("countdown").innerHTML = daysArg + " días ";
-    } else {
-      document.getElementById("countdown").innerHTML = "";
-      var element = document.querySelector(".arg");
-      element.classList.add("lugarVisitado");
-    }
+//   // Resultado para Argentina
+//     let daysArg = days - 21;
+//     if (daysArg > 0) {
+//       document.getElementById("countdown").innerHTML = daysArg + " días ";
+//     } else {
+//       document.getElementById("countdown").innerHTML = "";
+//       var element = document.querySelector(".arg");
+//       element.classList.add("lugarVisitado");
+//     }
   
-    // Resultado para Hawaii
-    let daysHaw = days - 20;
-    if (daysHaw > 0) {
-      document.getElementById("countdownH").innerHTML = daysHaw + " días ";
-    } else {
-      document.getElementById("countdownH").innerHTML = "";
-      var element = document.querySelector(".haw");
-      element.classList.remove("lugarVisitado");
-    }
-    //document.getElementById("countdownH").innerHTML = (days - 20) + " días ";
+//     // Resultado para Hawaii
+//     let daysHaw = days - 20;
+//     if (daysHaw > 0) {
+//       document.getElementById("countdownH").innerHTML = daysHaw + " días ";
+//     } else {
+//       document.getElementById("countdownH").innerHTML = "";
+//       var element = document.querySelector(".haw");
+//       element.classList.remove("lugarVisitado");
+//     }
+//     //document.getElementById("countdownH").innerHTML = (days - 20) + " días ";
 
-      // Resultado pala Los Angeles
-    let daysLos = days - 12;
-    if (daysLos > 0) {
-      document.getElementById("countdownA").innerHTML = daysLos + " días ";
-    } else {
-      document.getElementById("countdownA").innerHTML = "";
-      var element = document.querySelector(".loa");
-      element.classList.remove("lugarVisitado");
-      document.querySelector(".haw").classList.add("lugarVisitado");
-    }
-  //document.getElementById("countdownA").innerHTML = (days - 12) + " días";
+//       // Resultado pala Los Angeles
+//     let daysLos = days - 12;
+//     if (daysLos > 0) {
+//       document.getElementById("countdownA").innerHTML = daysLos + " días ";
+//     } else {
+//       document.getElementById("countdownA").innerHTML = "";
+//       var element = document.querySelector(".loa");
+//       element.classList.remove("lugarVisitado");
+//       document.querySelector(".haw").classList.add("lugarVisitado");
+//     }
+//   //document.getElementById("countdownA").innerHTML = (days - 12) + " días";
 
-    // Resultado para Las Vegas
-    let daysLas = days - 9;
-    if (daysLas > 0) {
-      document.getElementById("countdownV").innerHTML = daysLas + " días ";
-    } else {
-      document.getElementById("countdownV").innerHTML = "";
-      var element = document.querySelector(".las");
-      element.classList.remove("lugarVisitado");
-      document.querySelector(".loa").classList.add("lugarVisitado");
-    }
-     //document.getElementById("countdownV").innerHTML = (days - 9) + " días";
+//     // Resultado para Las Vegas
+//     let daysLas = days - 9;
+//     if (daysLas > 0) {
+//       document.getElementById("countdownV").innerHTML = daysLas + " días ";
+//     } else {
+//       document.getElementById("countdownV").innerHTML = "";
+//       var element = document.querySelector(".las");
+//       element.classList.remove("lugarVisitado");
+//       document.querySelector(".loa").classList.add("lugarVisitado");
+//     }
+//      //document.getElementById("countdownV").innerHTML = (days - 9) + " días";
 
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "";
-  }
-}, 1000);
+//   // If the count down is finished, write some text
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("countdown").innerHTML = "";
+//   }
+// }, 1000);
 
 
 
